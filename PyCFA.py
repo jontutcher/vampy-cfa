129c2436eea3b3f9663c2a442b73b50eb212fb61
Jon Tutcher <jon.tutcher@bbc.co.uk>
2015-04-10 17:21:27 +0100

    Added timestamps to CFA analysis windows

--- PyCFA.py
+++ PyCFA.py
@@ -1,8 +1,8 @@
+import numpy,vampy
 from yaafelib import *
-import numpy
+from vampy import *
 
 class PyCFA: 
-	
 	def __init__(self,inputSampleRate): 
 		self.m_inputSampleRate = inputSampleRate 
 		self.m_stepSize = 0
@@ -16,6 +16,16 @@ class PyCFA:
 		self.m_channels = channels
 		self.m_stepSize = stepSize		
 		self.m_blockSize = blockSize
+
+		# set feature plan at startup
+		self.fp = FeaturePlan(sample_rate=self.m_inputSampleRate)
+		self.fp.addFeature('cfa: ContinuousFrequencyActivation '\
+		'BinThreshold=0.1  FFTLength=0 FFTWindow=Hanning  NbPeaks=5 '\
+		'NbRunAvgFrames=21 NbSumFrames=100  NormWindow=Hanning '\
+		'StepNbSumFrames=50 blockSize={0} stepSize={1}'.format(blockSize, stepSize))
+		self.engine = Engine()
+		self.engine.load(self.fp.getDataFlow())
+		self.engine.reset()
 		return True
 	
 	def getMaker(self):
@@ -40,19 +50,12 @@ class PyCFA:
 		'identifier':'cfa',
 		'name':'Continuous Frequency Activation',
 		'description':'Continuously (over several seconds) activated '\
-                'frequency bands are detected and their spectral peak values '\
-                'summed up to the CFA-value',
+				'frequency bands are detected and their spectral peak values '\
+				'summed up to the CFA-value',
 		'unit':' ',
-		'hasFixedBinCount':True,
-		'binCount':1,
-		#'binNames':['1 Hz',1.5,'2 Hz',3,'4 Hz'],
 		'hasKnownExtents':False,
-		#'minValue':0.0,
-		#'maxValue':0.0,
 		'isQuantized':False,
-		#'quantizeStep':1.0,
-		'sampleType':'FixedSampleRate'
-		#'sampleRate':48000.0
+		'sampleType':'VariableSampleRate'
 		}
 
 		return [output0]
@@ -75,14 +78,6 @@ class PyCFA:
 	def setParameter(self,paramid,newval):
 		if paramid == 'threshold' :
 			self.threshold = newval
-                self.fp = FeaturePlan(sample_rate=self.m_inputSampleRate)
-                self.fp.addFeature('cfa: ContinuousFrequencyActivation '\
-                'BinThreshold=0.1  FFTLength=0 FFTWindow=Hanning  NbPeaks=5 '\
-                'NbRunAvgFrames=21 NbSumFrames=100  NormWindow=Hanning '\
-                'StepNbSumFrames=50 blockSize=1024  stepSize=1024')
-                self.engine = Engine()
-                self.engine.load(self.fp.getDataFlow())
-                self.engine.reset()
 		return
 
 		
@@ -95,34 +90,22 @@ class PyCFA:
 
 	# legacy process type: the input is a python list of samples
 	def process(self,inbuf,timestamp):
-            self.engine.writeInput('audio', numpy.array(inbuf))
-            self.engine.process()
-            #feats = self.engine.readAllOutputs()
-
-            #output0=[]
-            #results = feats['cfa'].tolist()
-
-            #for result in results:
-                #feature0={
-		#'hasTimestamp':False,		
-		#'values':[result],
-		#'label':str(result)				
-		#}
-		#output0.append(feature0)
-
-            return []
+		self.engine.writeInput('audio', numpy.array(inbuf))
+		self.engine.process()
+		return []
 
 	def getRemainingFeatures(self):
-            self.engine.flush()
-            feats = self.engine.readAllOutputs()
-
-            output0=[]
-            results = feats['cfa'].tolist()
-            for i in range(len(results)):
-                feature0={
-                'hasTimestamp':False,
-                'values':[results[i]],
-                'label':str(results[i])				
-                }
-                output0.append(feature0)
-            return [output0]
+		outputFeatureSet = FeatureSet()
+		outputFeatureSet[0] = flist = FeatureList()
+		self.engine.flush()
+		feats = self.engine.readAllOutputs()
+		featlength=50*self.m_stepSize	# 50 is the number of sub frames in Yaafe CFA
+		output0=[]
+		results = feats['cfa'].tolist()
+		for i in range(len(results)):
+			f = Feature()
+			f.hasTimestamp = True
+			f.timestamp = frame2RealTime(featlength*i,self.m_inputSampleRate)
+			f.values = [results[i]]
+			flist.append(f)	
+		return outputFeatureSet
