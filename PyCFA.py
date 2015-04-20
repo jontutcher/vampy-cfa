6015424dbee4fd63b111d711aa21c1def6aade82
Jon Tutcher <jon.tutcher@bbc.co.uk>
2015-04-20 12:20:02 +0100

    Implemented more CFA parameters, abstracted feature plan setting

--- PyCFA.py
+++ PyCFA.py
@@ -1,111 +1,178 @@
-import numpy,vampy
+import numpy
+import vampy
 from yaafelib import *
 from vampy import *
 
-class PyCFA: 
-	def __init__(self,inputSampleRate): 
-		self.m_inputSampleRate = inputSampleRate 
-		self.m_stepSize = 0
-		self.m_blockSize = 0
-		self.m_channels = 0
-		self.previousSample = 0.0
-		self.threshold = 0.005
-		self.counter = 0
-		
-	def initialise(self,channels,stepSize,blockSize):
-		self.m_channels = channels
-		self.m_stepSize = stepSize		
-		self.m_blockSize = blockSize
 
-		# set feature plan at startup
-		self.fp = FeaturePlan(sample_rate=self.m_inputSampleRate)
-		self.fp.addFeature('cfa: ContinuousFrequencyActivation '\
-		'BinThreshold=0.1  FFTLength=0 FFTWindow=Hanning  NbPeaks=5 '\
-		'NbRunAvgFrames=21 NbSumFrames=100  NormWindow=Hanning '\
-		'StepNbSumFrames=50 blockSize={0} stepSize={1}'.format(blockSize, stepSize))
-		self.engine = Engine()
-		self.engine.load(self.fp.getDataFlow())
-		self.engine.reset()
-		return True
-	
-	def getMaker(self):
-		return 'BBC'
-	
-	def getName(self):
-		return 'Continuous Frequency Activation'
-		
-	def getIdentifier(self):
-		return 'vampy-cfa'
-	
-	def getMaxChannelCount(self):
-		return 1
-		
-	def getInputDomain(self):
-		return 'TimeDomain'
-			
-	def getOutputDescriptors(self):
-		
-		#descriptors can be returned as python dictionaries
-		output0={
-		'identifier':'cfa',
-		'name':'Continuous Frequency Activation',
-		'description':'Continuously (over several seconds) activated '\
-				'frequency bands are detected and their spectral peak values '\
-				'summed up to the CFA-value',
-		'unit':' ',
-		'hasKnownExtents':False,
-		'isQuantized':False,
-		'sampleType':'VariableSampleRate'
-		}
+class PyCFA:
+    """
+    VamPy wrapper around 'cba-yaafe-extension'
+    continuous frequencyactivation plugin
+    """
 
-		return [output0]
+    def __init__(self, inputSampleRate):
+        self.m_inputSampleRate = inputSampleRate
+        self.m_stepSize = 0
+        self.m_blockSize = 0
+        self.m_channels = 0
+        self.previousSample = 0.0
+        params = self.getParameterDescriptors()
+        for param in params:
+            setattr(self, param['identifier'], param['defaultValue'])
 
+    def initialise(self, channels, stepSize, blockSize):
+        self.m_channels = channels
+        self.m_stepSize = stepSize
+        self.m_blockSize = blockSize
 
-	def getParameterDescriptors(self):
-		paramlist1={
-		'identifier':'threshold',
-		'name':'Noise threshold',
-		'description':'',
-		'unit':'v',
-		'minValue':0.0,
-		'maxValue':0.5,
-		'defaultValue':0.005,
-		'isQuantized':False
-		}
-		return [paramlist1]
+        # set feature plan at startup based on defaults
+        self.setFeaturePlan()
+        return True
 
+    def getMaker(self):
+        return 'BBC'
 
-	def setParameter(self,paramid,newval):
-		if paramid == 'threshold' :
-			self.threshold = newval
-		return
+    def getName(self):
+        return 'Continuous Frequency Activation'
 
-		
-	def getParameter(self,paramid):
-		if paramid == 'threshold' :
-			return self.threshold
-		else:
-			return 0.0
+    def getIdentifier(self):
+        return 'vampy-cfa'
 
+    def getMaxChannelCount(self):
+        return 1
 
-	# legacy process type: the input is a python list of samples
-	def process(self,inbuf,timestamp):
-		self.engine.writeInput('audio', numpy.array(inbuf))
-		self.engine.process()
-		return []
+    def getInputDomain(self):
+        return 'TimeDomain'
 
-	def getRemainingFeatures(self):
-		outputFeatureSet = FeatureSet()
-		outputFeatureSet[0] = flist = FeatureList()
-		self.engine.flush()
-		feats = self.engine.readAllOutputs()
-		featlength=50*self.m_stepSize	# 50 is the number of sub frames in Yaafe CFA
-		output0=[]
-		results = feats['cfa'].tolist()
-		for i in range(len(results)):
-			f = Feature()
-			f.hasTimestamp = True
-			f.timestamp = frame2RealTime(featlength*i,self.m_inputSampleRate)
-			f.values = [results[i]]
-			flist.append(f)	
-		return outputFeatureSet
+    def getOutputDescriptors(self):
+        #descriptors can be returned as python dictionaries
+        output0 = {
+            'identifier': 'cfa',
+            'name': 'Continuous Frequency Activation',
+            'description': 'Continuously (over several seconds) activated '
+                    'frequency bands are detected and their spectral peak '
+                    'values summed up to the CFA-value',
+            'unit': ' ',
+            'hasKnownExtents': False,
+            'isQuantized': False,
+            'sampleType': 'VariableSampleRate'
+        }
+
+        return [output0]
+
+    def setParameter(self, paramid, newval):
+        if paramid in list(param['identifier']
+                           for param in self.getParameterDescriptors()):
+            setattr(self, paramid, newval)
+
+        # update feature plan if necessary
+        self.setFeaturePlan()
+        return
+
+    def getParameter(self, paramid):
+        if paramid in list(param['identifier']
+                           for param in self.getParameterDescriptors()):
+            return getattr(self, paramid)
+        else:
+            return 0.0
+
+    # legacy process type: the input is a python list of samples
+    def process(self, inbuf, timestamp):
+        self.engine.writeInput('audio', numpy.array(inbuf))
+        self.engine.process()
+        return []
+
+    def getRemainingFeatures(self):
+        outputFeatureSet = FeatureSet()
+        outputFeatureSet[0] = flist = FeatureList()
+        self.engine.flush()
+        feats = self.engine.readAllOutputs()
+        featlength = int(self.sumFrames) * self.m_stepSize
+        results = feats['cfa'].tolist()
+        for i in range(len(results)):
+            f = Feature()
+            f.hasTimestamp = True
+            f.timestamp = frame2RealTime(featlength*i, self.m_inputSampleRate)
+            f.values = [results[i]]
+            flist.append(f)
+        return outputFeatureSet
+
+    def setFeaturePlan(self):
+        # prevent changing features if we don't yet have a step/block size
+        if self.m_stepSize <= 0:
+            return
+        if self.m_blockSize <= 0:
+            return
+        # build new feature plan
+        self.fp = FeaturePlan(sample_rate=self.m_inputSampleRate)
+        self.fp.addFeature('cfa: ContinuousFrequencyActivation '
+                           'BinThreshold={0:.2f}  FFTLength=0 '
+                           'FFTWindow=Hanning NbPeaks={1:.0f} '
+                           'NbRunAvgFrames={2:.0f} NbSumFrames={3:.0f} '
+                           'NormWindow=Hanning StepNbSumFrames={4:.0f} '
+                           'blockSize={5:.0f} stepSize={6:.0f}'.format(
+                               self.binThreshold,
+                               self.peaks,
+                               self.runAvgFrames,
+                               self.sumFrames,
+                               self.stepFrames,
+                               self.m_blockSize,
+                               self.m_stepSize))
+        self.engine = Engine()
+        self.engine.load(self.fp.getDataFlow())
+        self.engine.reset()
+        return
+
+    def getParameterDescriptors(self):
+        return [
+            {
+                'identifier': 'binThreshold',
+                'name': 'Bin Threshold',
+                'description': 'Values less than BinThreshold '
+                'will be set to 0, others to 1',
+                'minValue': 0.0,
+                'maxValue': 1,
+                'defaultValue': 0.1,
+                'isQuantized': False
+            },
+            {
+                'identifier': 'peaks',
+                'name': 'Peaks',
+                'description': 'Number of peaks to find',
+                'minValue': 1,
+                'maxValue': 20,
+                'defaultValue': 5,
+                'isQuantized': True,
+                'quantizeStep': 1.0
+            },
+            {
+                'identifier': 'runAvgFrames',
+                'name': 'Running Average Frames',
+                'description': 'Number of frames to integrate '
+                'for running average',
+                'minValue': 1,
+                'maxValue': 1000,
+                'defaultValue': 21,
+                'isQuantized': True,
+                'quantizeStep': 1.0
+            },
+            {
+                'identifier': 'sumFrames',
+                'name': 'Summing Frames',
+                'description': 'Number of frames to sum up',
+                'minValue': 1,
+                'maxValue': 1000,
+                'defaultValue': 100,
+                'isQuantized': True,
+                'quantizeStep': 1.0
+            },
+            {
+                'identifier': 'stepFrames',
+                'name': 'Step Size',
+                'description': 'Step between consecutive frames',
+                'minValue': 1,
+                'maxValue': 2048,
+                'defaultValue': 512,
+                'isQuantized': True,
+                'quantizeStep': 1.0
+            }]
